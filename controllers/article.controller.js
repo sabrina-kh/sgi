491cd76963e80ab660dedb3a958208d5a7204535
sabrina-kh <khlifasabrina96@gmail.com>
2022-05-27 19:14:35 +0100

    14th commit controllers

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -21,5 +21,53 @@ const addProduct = async (req,res) => {
     res.status(500).send("Nous avons pas pu se connecter au serveur !");
   }
 }
+// afficher liste des articles
+const getArticleList = async (req,res) => {
+  try {
+       const articleList = await Article.find().populate('respVente','userType')
+      res.json(articleList)
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Nous avons pas pu se connecter au serveur !");
+      
+  }
+}
+// afficher un article par id
+const getArticleById = async (req,res) => {
+  try {
+      const article = await Article.findById(req.params.articleId).populate('user','userType')
+res.json(article)
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Nous avons pas pu se connecter au serveur !");
+  }
+}
+// supprimer article 
+const deleteArticle = async (req,res) => {
+  try {
+      let article = await Article.findById(req.params.articleId)
+      if (!article) {
+          return res.json({error: "article introuvable "});
+      }
 
-module.exports = { addProduct }
+      article = await Article.findByIdAndRemove(req.params.articleId)
+      res.json({message: "article supprimé "})
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Nous avons pas pu se connecter au serveur !");
+      
+  }
+}
+
+// modifier article 
+const updateArticle = async (req,res) => {
+  
+  try {
+      
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Nous avons pas pu se connecter au serveur !"); 
+  }
+}
+
+module.exports = { addProduct,getArticleList, getArticleById, deleteArticle }
